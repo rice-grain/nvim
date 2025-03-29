fae574826a178edbe0aabc5bb8da76b336ab028b
EOussama <oussama.essamadi@gmail.com>
2025-03-29 18:27:39 +0000

    feat: added default config

---
+++ src/lua/core/defaults.lua
@@ -0,0 +1,17 @@
+-- Lines
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Spacing
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- UI
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+
+-- Splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
