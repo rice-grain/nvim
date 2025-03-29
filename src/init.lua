17ff7de0065cfdf4b3f8eaa1a60142691520636e
EOussama <oussama.essamadi@gmail.com>
2025-03-29 18:42:31 +0000

    feat: added lazy.nvim as a package manager

--- src/init.lua
+++ src/init.lua
@@ -1,2 +1,12 @@
+-- Loading Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "cp", "-r", vim.fn.stdpath("config") .. "/lazy", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Loading configurations
 require("core")
 require("plugins")
