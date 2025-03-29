b51cfce2c3508e2563f156c6b43e01510dc57f7b
EOussama <oussama.essamadi@gmail.com>
2025-03-29 18:27:49 +0000

    feat: added default keymaps

---
+++ src/lua/core/keymaps.lua
@@ -0,0 +1,9 @@
+-- General
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>w", ":w<CR>", {desc = "Save File"})
+vim.keymap.set("n", "<leader>q", ":q<CR>", {desc = "Quit Neovim"})
+
+-- Terminal keymaps
+vim.keymap.set("n", "<leader>th", ":split | terminal<CR>", {desc = "Open Horizontal Terminal"})
+vim.keymap.set("n", "<leader>tv", ":vsplit | terminal<CR>", {desc = "Open Vertical Terminal"})
+vim.keymap.set("n", "<leader>tf", ":terminal<CR>", {desc = "Open Full Terminal"})
