f7d9fd1e7f01042d9d205dc3bd7c911f066cbc17
EOussama <oussama.essamadi@gmail.com>
2025-03-30 21:43:50 +0000

    feat: added explorer keymaps

--- src/after/plugin/tree.lua
+++ src/after/plugin/tree.lua
@@ -1,3 +1,4 @@
 require("nvim-tree").setup({})
 
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {desc = "Toggle File Explorer"})
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+vim.keymap.set("n", "<leader>E", ":NvimTreeFindFile<CR>", { desc = "Focus File Explorer" })
