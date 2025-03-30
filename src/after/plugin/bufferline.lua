0190a6268e52523d94265e9b0e835f1ae024b766
EOussama <oussama.essamadi@gmail.com>
2025-03-30 21:48:25 +0000

    feat: added tab close

--- src/after/plugin/bufferline.lua
+++ src/after/plugin/bufferline.lua
@@ -1,4 +1,5 @@
 require("bufferline").setup({})
 
+vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { desc = "Close Buffer" })
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", {desc = "Next Buffer"})
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", {desc = "Previous Buffer"})
