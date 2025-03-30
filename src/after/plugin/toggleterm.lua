6aaa14e413249459310bc4dfc430ef64edc34c2f
EOussama <oussama.essamadi@gmail.com>
2025-03-30 22:10:59 +0000

    refact: singlr quote

--- src/after/plugin/toggleterm.lua
+++ src/after/plugin/toggleterm.lua
@@ -1,8 +1,8 @@
 require("toggleterm").setup({
   size = 20,
   open_mapping = [[<c-\>]],
-  direction = 'horizontal',
-  float_opts = { border = 'curved' }
+  direction = "horizontal",
+  float_opts = { border = "curved" }
 })
 
 vim.keymap.set("n", "<leader>tp", ":ToggleTerm direction=float<CR>", {desc = "Open Floating Terminal"})
--- src/lua/plugins/toggleterm.lua
+++ src/lua/plugins/toggleterm.lua
@@ -4,8 +4,8 @@ return {
     require("toggleterm").setup({
       size = 20,
       open_mapping = [[<c-\>]],
-      direction = 'horizontal',
-      float_opts = { border = 'curved' }
+      direction = "horizontal",
+      float_opts = { border = "curved" }
    })
 
     vim.keymap.set("n", "<leader>tp", ":ToggleTerm direction=float<CR>", {desc = "Open Floating Terminal"})
