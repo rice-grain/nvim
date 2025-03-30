3d2edb8b298e0175a7c77174de9c4e66f18ad30b
EOussama <oussama.essamadi@gmail.com>
2025-03-30 22:00:43 +0000

    feat: added terminal shortcuts

--- src/after/plugin/toggleterm.lua
+++ src/after/plugin/toggleterm.lua
@@ -6,3 +6,12 @@ require("toggleterm").setup({
 })
 
 vim.keymap.set("n", "<leader>tp", ":ToggleTerm direction=float<CR>", {desc = "Open Floating Terminal"})
+vim.keymap.set({ "n", "v", "i" }, "<A-v>", ":ToggleTerm direction=vertical<CR>", { desc = "Open Vertical Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-h>", ":ToggleTerm direction=horizontal<CR>", { desc = "Open Horizontal Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-i>", ":ToggleTerm direction=float<CR>", { desc = "Open Floating Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-t>", ":ToggleTerm direction=tab<CR>", { desc = "Open Full Terminal" })
+
+vim.keymap.set("t", "<A-v>", "<C-\\><C-n>:ToggleTerm direction=vertical<CR>", { desc = "Open Vertical Terminal" })
+vim.keymap.set("t", "<A-h>", "<C-\\><C-n>:ToggleTerm direction=horizontal<CR>", { desc = "Open Horizontal Terminal" })
+vim.keymap.set("t", "<A-i>", "<C-\\><C-n>:ToggleTerm direction=float<CR>",   { desc = "Open Floating Terminal" })
+vim.keymap.set("t", "<A-t>", "<C-\\><C-n>:ToggleTerm direction=tab<CR>",      { desc = "Open Full Terminal" })
