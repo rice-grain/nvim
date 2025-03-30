b40abf49ac5da45c46bfbb337a3bc77229b06d33
EOussama <oussama.essamadi@gmail.com>
2025-03-30 06:06:35 +0000

    feat: added file explorer

--- src/lua/plugins/init.lua
+++ src/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 require("lazy").setup({
   { import = "plugins.theme" },
   { import = "plugins.mason" },
+  { import = "plugins.explorer" },
   { import = "plugins.toggleterm" },
   { import = "plugins.treesitter" },
 })
