6c79a52c5f8c9e2de5f5cc8fa385dc562ace2f5e
eoussama <oussama_gtacj@live.fr>
2025-04-02 21:41:03 +0000

    feat: added shortcut for hiding highlight

--- src/lua/core/keymaps.lua
+++ src/lua/core/keymaps.lua
@@ -1,7 +1,9 @@
 -- General
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>w", ":w<CR>", {desc = "Save File"})
 vim.keymap.set("n", "<leader>q", ":q<CR>", {desc = "Quit Neovim"})
+vim.keymap.set("n", "<esc>", ":noh<CR>", {desc = "Hide Highlight"})
 
 -- Terminal keymaps
 vim.keymap.set("n", "<leader>th", ":split | terminal<CR>", {desc = "Open Horizontal Terminal"})
