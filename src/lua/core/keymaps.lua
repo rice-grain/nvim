7ffbb5e4687406ad88888c298e741854d9c32bca
eoussama <oussama_gtacj@live.fr>
2025-04-04 20:25:46 +0000

    feat: added terminal mode exit shortcut

--- src/lua/core/keymaps.lua
+++ src/lua/core/keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", {desc = "Quit Neovim"})
 vim.keymap.set("n", "<esc>", "<cmd>noh<CR>", {desc = "Hide Highlight"})
 
 -- Terminal keymaps
-vim.keymap.set("n", "<leader>th", "<cmd>split | terminal<CR>", {desc = "Open Horizontal Terminal"})
-vim.keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", {desc = "Open Vertical Terminal"})
 vim.keymap.set("n", "<leader>tf", "<cmd>terminal<CR>", {desc = "Open Full Terminal"})
+vim.keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", {desc = "Open Vertical Terminal"})
+vim.keymap.set("n", "<leader>th", "<cmd>split | terminal<CR>", {desc = "Open Horizontal Terminal"})
+vim.keymap.set("t", "<C-w>n", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Exit terminal mode" })
