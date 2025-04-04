4ab1ac1ba14046b98359c35b6bef30da6c198dca
eoussama <oussama_gtacj@live.fr>
2025-04-04 20:31:17 +0000

    fix: removed slow mouse scrolling

--- src/after/plugin/mini.lua
+++ src/after/plugin/mini.lua
@@ -1,7 +1,7 @@
 require("mini.move").setup()
 require("mini.pairs").setup()
 require("mini.notify").setup()
-require("mini.animate").setup()
 require("mini.starter").setup()
 require('mini.cursorword').setup()
 require("mini.indentscope").setup()
+require("mini.animate").setup({ scroll = { enable = false } })
