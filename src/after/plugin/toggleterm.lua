1f60b7a1c929a36dc434ed5de3ce383c80a26698
eoussama <oussama_gtacj@live.fr>
2025-04-04 20:25:58 +0000

    fix: separated terminal instances

--- src/after/plugin/toggleterm.lua
+++ src/after/plugin/toggleterm.lua
@@ -8,16 +8,61 @@ require("toggleterm").setup({
   end,
   open_mapping = [[<c-\>]],
   direction = "horizontal",
-  float_opts = { border = "curved" }
+  float_opts = { border = "curved" },
 })
 
-vim.keymap.set("n", "<leader>tp", "<cmd>ToggleTerm direction=float<CR>", {desc = "Open Floating Terminal"})
-vim.keymap.set({ "n", "v", "i" }, "<A-v>", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Open Vertical Terminal" })
-vim.keymap.set({ "n", "v", "i" }, "<A-h>", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Open Horizontal Terminal" })
-vim.keymap.set({ "n", "v", "i" }, "<A-i>", "<cmd>ToggleTerm direction=float<CR>", { desc = "Open Floating Terminal" })
-vim.keymap.set({ "n", "v", "i" }, "<A-t>", "<cmd>ToggleTerm direction=tab<CR>", { desc = "Open Full Terminal" })
+local Terminal  = require("toggleterm.terminal").Terminal
 
-vim.keymap.set("t", "<A-v>", "<C-\\><C-n><cmd>ToggleTerm direction=vertical<CR>", { desc = "Open Vertical Terminal" })
-vim.keymap.set("t", "<A-h>", "<C-\\><C-n><cmd>ToggleTerm direction=horizontal<CR>", { desc = "Open Horizontal Terminal" })
-vim.keymap.set("t", "<A-i>", "<C-\\><C-n><cmd>ToggleTerm direction=float<CR>",   { desc = "Open Floating Terminal" })
-vim.keymap.set("t", "<A-t>", "<C-\\><C-n><cmd>ToggleTerm direction=tab<CR>",      { desc = "Open Full Terminal" })
+local horizontal_terminal = Terminal:new({
+  direction = "horizontal",
+  size = function(term)
+    return math.max(20, math.floor(vim.o.lines * 0.3))
+  end,
+  hidden = true,
+})
+
+local vertical_terminal = Terminal:new({
+  direction = "vertical",
+  size = function(term)
+    return math.max(40, math.floor(vim.o.columns * 0.3))
+  end,
+  hidden = true,
+})
+
+local float_terminal = Terminal:new({
+  direction = "float",
+  float_opts = { border = "curved" },
+  hidden = true,
+})
+
+local tab_terminal = Terminal:new({
+  direction = "tab",
+  hidden = true,
+})
+
+function _G.toggle_horizontal_terminal()
+  horizontal_terminal:toggle()
+end
+
+function _G.toggle_vertical_terminal()
+  vertical_terminal:toggle()
+end
+
+function _G.toggle_float_terminal()
+  float_terminal:toggle()
+end
+
+function _G.toggle_tab_terminal()
+  tab_terminal:toggle()
+end
+
+vim.keymap.set("n", "<leader>tp", function() _G.toggle_float_terminal() end, { desc = "Open Floating Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-v>", function() _G.toggle_vertical_terminal() end, { desc = "Open Vertical Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-h>", function() _G.toggle_horizontal_terminal() end, { desc = "Open Horizontal Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-i>", function() _G.toggle_float_terminal() end, { desc = "Open Floating Terminal" })
+vim.keymap.set({ "n", "v", "i" }, "<A-t>", function() _G.toggle_tab_terminal() end, { desc = "Open Full Terminal" })
+
+vim.keymap.set("t", "<A-v>", "<C-\\><C-n>:lua toggle_vertical_terminal()<CR>", { desc = "Open Vertical Terminal" })
+vim.keymap.set("t", "<A-h>", "<C-\\><C-n>:lua toggle_horizontal_terminal()<CR>", { desc = "Open Horizontal Terminal" })
+vim.keymap.set("t", "<A-i>", "<C-\\><C-n>:lua toggle_float_terminal()<CR>",   { desc = "Open Floating Terminal" })
+vim.keymap.set("t", "<A-t>", "<C-\\><C-n>:lua toggle_tab_terminal()<CR>",      { desc = "Open Full Terminal" })
