1ebcbe4db11c282689d6dab6f4ac78dac97f76a2
eoussama <oussama_gtacj@live.fr>
2025-04-06 17:10:06 +0100

    feat: added auto tree update

--- src/after/plugin/tree.lua
+++ src/after/plugin/tree.lua
@@ -1,4 +1,9 @@
-require("nvim-tree").setup({})
+require("nvim-tree").setup({
+  update_focused_file = {
+    enable      = true,
+    update_root = true,
+  }
+})
 
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
 vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeFindFile<CR>", { desc = "Focus File Explorer" })
